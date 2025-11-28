25b010895a5a4e686e97788ee595e7a510d58b60
Piotr-Kniaz <piotr.kniaz@ya.ru>
2025-11-29 00:12:59 +0300

    Update repository link

--- HBDPG-2.Core/Result.cs
+++ HBDPG-2.Core/Result.cs
@@ -1,7 +1,7 @@
 /*  Copyright (C) 2025 Piotr Kniaz
 
     This file is part of HBDPG-2.
-    Repository: https://github.com/HBDPG-2/hbdpg-2-tui
+    Repository: https://github.com/HBDPG-2/hbdpg-2-core
 
     Licensed under the MIT License. See LICENSE file in the project root for details.
 */
